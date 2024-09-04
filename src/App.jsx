9a1d6ceeeb3ebafb7f53ed31247ed86f8d1405dc
Sanskar7964 <sanskar123.sg@gmail.com>
2024-09-04 17:16:56 +0530

    frontend

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { SignUp } from "./pages/SignUp";
+import { SignIn } from "./pages/SignIn";
+import { Dashboard } from "./pages/Dashboard";
+import { SendMoney } from "./pages/SendMoney";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<SendMoney />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
